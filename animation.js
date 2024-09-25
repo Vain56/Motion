5587290f2be589d5c7323bdc7c2aedb298b0746a
Kilian Lüdemann <kilian.luedeman@realcore.de>
2024-09-25 13:36:28 +0200

    ADD - use library popmotion

---
+++ animation.js
@@ -0,0 +1,12 @@
+const ball = document.querySelector(".ball");
+
+popmotion.animate({
+    from: "0px",
+    to: "150px",
+    repeat: Infinity,
+    repeatType: "mirror",
+    type: "spring",
+    onUpdate(update) {
+        ball.style.left = update;
+    }
+});
